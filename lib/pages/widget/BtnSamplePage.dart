e1ed495ab7f241304686e6bdeb5b39712916bc28
xuanguofeng <xuanguofeng@live.com>
2018-07-11 20:30:23 +0800

    添加部分按钮样式

--- lib/pages/widget/BtnSamplePage.dart
+++ lib/pages/widget/BtnSamplePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class BtnSamplePage extends StatefulWidget {
   @override
@@ -8,15 +9,107 @@ class BtnSamplePage extends StatefulWidget {
 }
 
 class BtnSamplePageState extends State<BtnSamplePage> {
+  // ignore: enum_in_class
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("BtnSamplePage"),
-      ),
-      body: Center(
-        child: Text("BtnSamplePage"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("BtnSamplePage"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            CupertinoButton(
+              child: Text('iOS风格按钮'),
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              onPressed: () {
+                print("ios按钮");
+              },
+            ),
+            CupertinoButton(
+              child: Text('iOS风格按钮(圆角)'),
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(Radius.circular(50.0)),
+              onPressed: () {
+                print("ios按钮");
+              },
+            ),
+            CupertinoButton(
+              child: Text('iOS风格按钮(圆角)'),
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(30.0),
+              pressedOpacity: 1.0,
+              onPressed: () {
+                print("ios按钮");
+              },
+            ),
+            RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('material风格按钮'),
+              onPressed: () {
+                print('11');
+              },
+            ),
+            FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                print('sss');
+              },
+            ),
+            FlatButton(
+              child: Text("material风格按钮"),
+              color: Colors.blue,
+              textColor: Colors.white,
+              onPressed: () {
+                print('sdsd');
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.directions_bike),
+              tooltip: 'Increase volume by 10%',
+              color: Colors.blue,
+              onPressed: () {
+                print(' ');
+              },
+            ), PopupMenuButton<WhyFarther>(
+              onSelected: (WhyFarther result) {
+                setState(() {
+
+                });
+              },
+              itemBuilder: (BuildContext context) =>
+              <PopupMenuEntry<WhyFarther>>[
+                const PopupMenuItem<WhyFarther>(
+                  value: WhyFarther.harder,
+                  child: const Text('Working a lot harder'),
+                ),
+                const PopupMenuItem<WhyFarther>(
+                  value: WhyFarther.smarter,
+                  child: const Text('Being a lot smarter'),
+                ),
+                const PopupMenuItem<WhyFarther>(
+                  value: WhyFarther.selfStarter,
+                  child: const Text('Being a self-starter'),
+                ),
+                const PopupMenuItem<WhyFarther>(
+                  value: WhyFarther.tradingCharter,
+                  child: const Text('Placed in charge of trading charter'),
+                ),
+              ],
+            )
+          ],
+        ));
   }
+}
+
+enum WhyFarther {
+  harder,
+  smarter,
+  selfStarter,
+  tradingCharter
 }
