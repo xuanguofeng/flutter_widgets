cde5fd71bad2b55a987076d041b4f29a7042459e
xuanguofeng <xuanguofeng@live.com>
2018-07-12 10:06:04 +0800

    按钮页面

--- lib/pages/widget/BtnSamplePage.dart
+++ lib/pages/widget/BtnSamplePage.dart
@@ -11,7 +11,6 @@ class BtnSamplePage extends StatefulWidget {
 class BtnSamplePageState extends State<BtnSamplePage> {
   // ignore: enum_in_class
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,96 +19,120 @@ class BtnSamplePageState extends State<BtnSamplePage> {
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            CupertinoButton(
-              child: Text('iOS风格按钮'),
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-              onPressed: () {
-                print("ios按钮");
-              },
+            Container(
+              margin: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: CupertinoButton(
+                child: Text('iOS风格按钮'),
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                onPressed: () {
+                  print("ios按钮");
+                },
+              ),
             ),
-            CupertinoButton(
-              child: Text('iOS风格按钮(圆角)'),
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(50.0)),
-              onPressed: () {
-                print("ios按钮");
-              },
+            Container(
+              margin: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: CupertinoButton(
+                child: Text('iOS风格按钮(圆角)'),
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                onPressed: () {
+                  print("ios按钮");
+                },
+              ),
             ),
-            CupertinoButton(
-              child: Text('iOS风格按钮(圆角)'),
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(30.0),
-              pressedOpacity: 1.0,
-              onPressed: () {
-                print("ios按钮");
-              },
+            Container(
+              margin: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: CupertinoButton(
+                child: Text('iOS风格按钮(圆角)'),
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(30.0),
+                pressedOpacity: 1.0,
+                onPressed: () {
+                  print("ios按钮");
+                },
+              ),
             ),
-            RaisedButton(
-              textColor: Colors.white,
-              color: Colors.blue,
-              child: Text('material风格按钮'),
-              onPressed: () {
-                print('11');
-              },
+            Container(
+              margin: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: RaisedButton(
+                textColor: Colors.white,
+                color: Colors.blue,
+                child: Text('material风格按钮'),
+                onPressed: () {
+                  print('11');
+                },
+              ),
             ),
-            FloatingActionButton(
-              child: Icon(Icons.add),
-              onPressed: () {
-                print('sss');
-              },
+            Container(
+              margin: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: () {
+                  print('sss');
+                },
+              ),
             ),
-            FlatButton(
-              child: Text("material风格按钮"),
-              color: Colors.blue,
-              textColor: Colors.white,
-              onPressed: () {
-                print('sdsd');
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.directions_bike),
-              tooltip: 'Increase volume by 10%',
-              color: Colors.blue,
-              onPressed: () {
-                print(' ');
-              },
-            ), PopupMenuButton<WhyFarther>(
-              onSelected: (WhyFarther result) {
-                setState(() {
-
-                });
-              },
-              itemBuilder: (BuildContext context) =>
-              <PopupMenuEntry<WhyFarther>>[
-                const PopupMenuItem<WhyFarther>(
-                  value: WhyFarther.harder,
-                  child: const Text('Working a lot harder'),
+            Container(
+              margin: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: Container(
+                child: FlatButton(
+                  child: Text("material风格按钮"),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    print('sdsd');
+                  },
                 ),
-                const PopupMenuItem<WhyFarther>(
-                  value: WhyFarther.smarter,
-                  child: const Text('Being a lot smarter'),
-                ),
-                const PopupMenuItem<WhyFarther>(
-                  value: WhyFarther.selfStarter,
-                  child: const Text('Being a self-starter'),
-                ),
-                const PopupMenuItem<WhyFarther>(
-                  value: WhyFarther.tradingCharter,
-                  child: const Text('Placed in charge of trading charter'),
-                ),
-              ],
-            )
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: IconButton(
+                icon: Icon(Icons.directions_bike),
+                tooltip: 'Increase volume by 10%',
+                color: Colors.blue,
+                onPressed: () {
+                  print(' ');
+                },
+              ),
+            ),
+            Container(
+              child: PopupMenuButton<Item>(
+                onSelected: (Item result) {
+                  setState(() {});
+                },
+                itemBuilder: (BuildContext context) => <PopupMenuEntry<Item>>[
+                      const PopupMenuItem<Item>(
+                        value: Item.item1,
+                        child: const Text('item 1'),
+                      ),
+                      const PopupMenuItem<Item>(
+                        value: Item.item2,
+                        child: const Text('item 2'),
+                      ),
+                      const PopupMenuItem<Item>(
+                        value: Item.item3,
+                        child: const Text('item 3'),
+                      ),
+                      const PopupMenuItem<Item>(
+                        value: Item.item4,
+                        child: const Text('item 4'),
+                      ),
+                    ],
+              ),
+            ),
           ],
         ));
   }
 }
 
-enum WhyFarther {
-  harder,
-  smarter,
-  selfStarter,
-  tradingCharter
-}
+enum Item { item1, item2, item3, item4 }
