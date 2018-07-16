7830e4b9a8d9f1905b8ca4ddcd6b1514c50ca08a
xuanguofeng <xuanguofeng@live.com>
2018-07-16 16:52:59 +0800

    文本页面、和dialog页面

--- lib/pages/widget/DialogSamplePage.dart
+++ lib/pages/widget/DialogSamplePage.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class DialogSamplePage extends StatefulWidget {
@@ -7,15 +8,198 @@ class DialogSamplePage extends StatefulWidget {
   }
 }
 
+enum Department { treasury, state }
+
 class DialogSamplePageState extends State<DialogSamplePage> {
+  Future<Null> _showSimpleDialog() async {
+    switch (await showDialog<Department>(
+        context: context,
+        builder: (BuildContext context) {
+          return new SimpleDialog(
+            title: const Text('弹窗title'),
+            children: <Widget>[
+              new SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, Department.treasury);
+                },
+                child: const Text('item one'),
+              ),
+              new SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, Department.state);
+                },
+                child: const Text('item two'),
+              ),
+            ],
+          );
+        })) {
+      case Department.treasury:
+        print('Department.treasury');
+        break;
+      case Department.state:
+        print('Department.state');
+        break;
+    }
+  }
+
+  Future<Null> _showAlertDialog() async {
+    return showDialog<Null>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return new AlertDialog(
+          title: new Text('AlertDialog'),
+          content: new SingleChildScrollView(
+            child: new ListBody(
+              children: <Widget>[
+                new Text('alertDialog内容'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text('确认'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<Null> _showSnackBar() async {
+    return ScaffoldState().showSnackBar(SnackBar(
+      content: Text("xuanguofeng"),
+      backgroundColor: Colors.blue,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("DialogSamplePage"),
       ),
-      body: Center(
-        child: Text("DialogSamplePage"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(20.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('SimpleDialog'),
+              onPressed: () {
+                _showSimpleDialog();
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('AlertDialog'),
+              onPressed: () {
+                _showAlertDialog();
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('timePicker'),
+              onPressed: () {
+                showTimePicker(context: context, initialTime: TimeOfDay.now());
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('DatePicker'),
+              onPressed: () {
+                showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2010),
+                    lastDate: DateTime.now());
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20.0),
+            alignment: Alignment.center,
+            child: RaisedButton(
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('bottomsheet'),
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            "这是一个bottomSheet",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Theme.of(context).accentColor,
+                                fontSize: 14.0),
+                          ),
+                        ),
+                      );
+                    });
+              },
+            ),
+          ),
+//          Container(
+//            margin: EdgeInsets.all(20.0),
+//            alignment: Alignment.center,
+//            child: RaisedButton(
+//              textColor: Colors.white,
+//              color: Colors.blue,
+//              child: Text('snackBar'),
+//              onPressed: () {
+//                Scaffold.of(context).showSnackBar(SnackBar(
+//                  content: Container(
+//                    child: Padding(
+//                      padding: const EdgeInsets.all(20.0),
+//                      child: Text(
+//                        "这是一个bottomSheet",
+//                        textAlign: TextAlign.center,
+//                        style: TextStyle(
+//                            color: Theme.of(context).accentColor,
+//                            fontSize: 14.0),
+//                      ),
+//                    ),
+//                  ),
+//                  backgroundColor: Colors.blue,
+//                  duration: Duration(seconds: 10),
+//                  action: SnackBarAction(
+//                      label: "sss",
+//                      onPressed: () {
+//                        print('ss');
+//                      }),
+//                ).build(context));
+//
+//
+////                ScaffoldState().showSnackBar(.build(context));
+//              },
+//            ),
+//          ),
+        ],
       ),
     );
   }
--- lib/pages/widget/TextSamplePage.dart
+++ lib/pages/widget/TextSamplePage.dart
@@ -14,9 +14,37 @@ class TextSamplePageState extends State<TextSamplePage> {
       appBar: AppBar(
         title: Text("TextSamplePage"),
       ),
-      body: Center(
-        child: Text("TextSamplePage"),
-      ),
+      body:
+          Column(
+              mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(20.0),
+          alignment: Alignment.center,
+          child: Text(
+            "hello,this is text",
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            style: new TextStyle(fontSize: 16.0, color: Colors.blue),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(20.0),
+          alignment: Alignment.center,
+          child: RichText(
+            text: TextSpan(
+              text: 'hello,this is RichText。',
+              style: TextStyle(fontSize: 16.0, color: Colors.red),
+              children: <TextSpan>[
+                TextSpan(
+                    text: 'bold',
+                    style: new TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.red)),
+                TextSpan(text: ' world!'),
+              ],
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
